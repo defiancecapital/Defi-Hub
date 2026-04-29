9c730b5313075b9dd8cf2ba5f370179684a51750
iamcapoate <iamcapote@gmail.com>
2026-04-29 13:08:16 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Background from "./components/Background";
+import Index from "./pages/Index";
+import Gaming from "./pages/Gaming";
+import DeFi from "./pages/DeFi";
+import NFTMarketplace from "./pages/NFTMarketplace";
+import Launchpad from "./pages/Launchpad";
+import Governance from "./pages/Governance";
+import NotFound from "./pages/NotFound";
+
+const App = () => (
+  <TooltipProvider>
+    <Background />
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/gaming" element={<Gaming />} />
+        <Route path="/defi" element={<DeFi />} />
+        <Route path="/nft-marketplace" element={<NFTMarketplace />} />
+        <Route path="/launchpad" element={<Launchpad />} />
+        <Route path="/governance" element={<Governance />} />
+        {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </TooltipProvider>
+);
+
+export default App;
